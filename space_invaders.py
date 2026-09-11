69a3a7816431a1ffc08de79bcabc6db94928ab88
rachhoya <virinarachhoya@gmail.com>
2026-09-11 19:00:57 +0100

    Update space_invaders.py

--- space_invaders.py
+++ space_invaders.py
@@ -18,6 +18,7 @@ red_spaceship=pygame.transform.rotate(pygame.transform.scale(spaceship2,(spacesh
 
 border=pygame.Rect(WIDTH//2-5,0,10,HEIGHT)
 vel=5
+fps=60
 
 def draw_window(red,yellow):
     screen.blit(space,(0,0))
@@ -27,45 +28,52 @@ def draw_window(red,yellow):
     pygame.display.update()
 
 def yellow_handle_movement(key_pressed,yellow):
-    if key_pressed[pygame.K_a]:
+    if key_pressed[pygame.K_a] and yellow.x>0:
         yellow.x-=vel
-    if key_pressed[pygame.K_d]:
+    if key_pressed[pygame.K_d] and yellow.x+vel+yellow.width<border.x:
             yellow.x+=vel
-    if key_pressed[pygame.K_w]:
+    if key_pressed[pygame.K_w] and yellow.y-vel>0:
             yellow.y-=vel
-    if key_pressed[pygame.K.s]:
+    if key_pressed[pygame.K_s] and yellow.y+vel+yellow.height<HEIGHT-15:
             yellow.y+=vel
 
 def red_handle_movement(key_pressed,red):
-    if key_pressed[pygame.K_LEFT]:
+    if key_pressed[pygame.K_LEFT] and red.x-vel>border.x+border.width:
         red.x-=vel
-    if key_pressed[pygame.K_RIGHT]:
+    if key_pressed[pygame.K_RIGHT] and red.x+vel+red.width<WIDTH:
             red.x+=vel
-    if key_pressed[pygame.K_UP]:
+    if key_pressed[pygame.K_UP] and red.y-vel>0:
             red.y-=vel
-    if key_pressed[pygame.K.DOWN]:
+    if key_pressed[pygame.K_DOWN] and red.y+vel+red.height<HEIGHT-15:
             red.y+=vel
+
+#def handle_bullets():
+      
     
-    
-
-
-
-
 
 
 
 
 def main():
     red=pygame.Rect(700,300,spaceship_width,spaceship_height)
-    yellow=pygame.Rect(700,300,spaceship_width,spaceship_height)
-    for event in pygame.event.get():
-        if event.type==pygame.QUIT:
-            pygame.quit()
-    key_pressed=pygame.key.get_pressed()
-    draw_window(red,yellow)
+    yellow=pygame.Rect(200,300,spaceship_width,spaceship_height)
 
+    red_bullets=[]
+    yellow_bullets=[]
 
+    red_health=10
+    yellow_health=10
 
-    pygame.display.update()
+    clock=pygame.time.Clock()
+
+    while True:
+        clock.tick(fps)
+        for event in pygame.event.get():
+            if event.type==pygame.QUIT:
+                pygame.quit()
+        key_pressed=pygame.key.get_pressed()
+        draw_window(red,yellow)
+        red_handle_movement(key_pressed,red)
+        yellow_handle_movement(key_pressed,yellow)
 
 main()
